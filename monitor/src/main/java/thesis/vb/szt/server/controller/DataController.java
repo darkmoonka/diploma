f1ce970fd56bd3c1a9715186352507adc856262e
Szakály Tibor <szakaly.tibi@gmail.com>
2013-10-16 20:56:56 +0200

    agent reg v1

--- monitor/src/main/java/thesis/vb/szt/server/controller/DataController.java
+++ monitor/src/main/java/thesis/vb/szt/server/controller/DataController.java
@@ -1,5 +1,6 @@
 package thesis.vb.szt.server.controller;
 
+import java.io.IOException;
 import java.net.URLDecoder;
 
 import org.apache.log4j.Logger;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import thesis.vb.szt.server.dao.Dao;
 import thesis.vb.szt.server.entity.Agent;
@@ -39,16 +42,18 @@ public class DataController
 
 	@RequestMapping(value = "/registerAgent", method = RequestMethod.POST)
 	public @ResponseBody
-	byte[] registerAgent(@RequestBody String macAddress, @RequestBody String publicKey)
+	byte[] registerAgent(@RequestParam("macAddress") String macAddress,
+			@RequestParam("publicKey") MultipartFile publicKey) throws IOException
 	{
 		Agent agent = dao.getAgentByAddress(macAddress);
-
+		byte[] key = publicKey.getBytes();
+		
 		if (agent != null) // már létezik a DBben
 			return null;
 
-		// TODO meadd agent to db and
+		// TODO add agent to db and
 		// return server's pub key
-		
+
 		return new byte[] { 3 };
 	}
 }
